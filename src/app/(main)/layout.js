c0c460aa2b7774a1d82180b607fb98a45ded1ab2
VuongTran <vuongtr15@gmail.com>
2025-03-22 10:38:19 +0700

    Refactor layout component to include logo in the main menu and enhance styling

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -3,7 +3,12 @@ import './layout.scss'
 export default function LayoutMain({ children }) {
     return (
         <div className="main-layout">
-            <div className="main-menu"></div>
+            <div className="main-menu">
+                <div className='logo'>
+                    <img src="/images/logo.png" alt="Logo" />
+                </div>
+
+            </div>
             <div className="main-body">
                 {children}
             </div>
