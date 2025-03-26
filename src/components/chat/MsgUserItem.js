2cd65fe134ec5bdcfee7b245c992bcfc4e1df761
Kito <kito.tran@reginamiracle.com>
2025-03-26 16:34:08 +0700

    Remove unused timestamp formatting in MsgSystemItem and MsgUserItem components

--- src/components/chat/MsgUserItem.js
+++ src/components/chat/MsgUserItem.js
@@ -4,9 +4,9 @@ export default function MsgUserItem({ message }) {
     return (<div key={message.id} className="message-container flex justify-end">
         <div className="message max-w-[70%] p-3 rounded-lg bg-blue-500 text-white rounded-tr-none">
             <div className="message-text">{formatMessageWithLineBreaks(message.text)}</div>
-            <div className="message-time text-xs mt-1 text-blue-100" >
+            {/* <div className="message-time text-xs mt-1 text-blue-100" >
                 {formatTime(message.timestamp)}
-            </div>
+            </div> */}
         </div>
     </div>);
 }
